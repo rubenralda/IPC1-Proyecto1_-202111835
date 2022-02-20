0f59dd5abdfc133b34267da83639b3d0f793fe21
rubenralda <rubenralda@gmail.com>
2022-02-20 16:29:43 -0600

    Creación del proyecto

---
+++ src/proyecto1_biblioteca/Proyecto1_biblioteca.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_biblioteca;
+
+/**
+ *
+ * @author ruben
+ */
+public class Proyecto1_biblioteca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal pantalla1= new Principal();
+        pantalla1.setVisible(true);
+    }
+    
+}
